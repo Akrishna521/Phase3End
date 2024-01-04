2bc5d275db025ff842ab3c6a698b601026b92bd2
Akrishna521 <anishetty.saikrishna@mphasis.com>
2024-01-04 19:52:39 +0530

    Phase3End

---
+++ Phase3End/Data/Phase3DbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Phase3End.model;
+
+namespace Phase3End.Data
+{
+    public class Phase3DbContext : DbContext
+    {
+        public Phase3DbContext (DbContextOptions<Phase3DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Phase3End.model.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<Phase3End.model.EmpProfile>? EmpProfile { get; set; }
+    }
+}
